f475e0cfd62661bb4cb75266b2149bcebf067922
jeronimo-jaramillor <123768592+jeronimo-jaramillor@users.noreply.github.com>
2023-02-10 14:02:18 -0500

    Varias cosas

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContenteLoaded", () => {
+  const cardAdj = [
+    {
+      name: "jun",
+      img: "images/jun.jpg"
+    }
+  ];
+
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+});
