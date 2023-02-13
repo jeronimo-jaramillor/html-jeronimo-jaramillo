875600023eb7c2f2a5bb6e4367c11ffe5641db81
jeronimo-jaramillor <123768592+jeronimo-jaramillor@users.noreply.github.com>
2023-02-13 12:33:37 -0500

    fotos

--- app.js
+++ app.js
@@ -3,6 +3,66 @@ document.addEventListener("DOMContenteLoaded", () => {
     {
       name: "jun",
       img: "images/jun.jpg"
+    },
+    {
+      name: "descarga",
+      img: "images/descarga.jpg"
+    },
+    {
+      name: "descargas",
+      img: "images/descargas.png"
+    },
+    {
+      name: "hu",
+      img: "images/hu.jpg"
+    },
+    {
+      name: "images",
+      img: "images/images.jpg"
+    },
+    {
+      name: "pales",
+      img: "images/pales.jpg"
+    },
+    {
+      name: "trueque",
+      img: "images/trueque.jpg"
+    },
+    {
+      name: "turc",
+      img: "images/turc.jpg"
+    },
+    {
+      name: "jun",
+      img: "images/jun.jpg"
+    },
+    {
+      name: "descarga",
+      img: "images/descarga.jpg"
+    },
+    {
+      name: "descargas",
+      img: "images/descargas.png"
+    },
+    {
+      name: "hu",
+      img: "images/hu.jpg"
+    },
+    {
+      name: "images",
+      img: "images/images.jpg"
+    },
+    {
+      name: "pales",
+      img: "images/pales.jpg"
+    },
+    {
+      name: "trueque",
+      img: "images/trueque.jpg"
+    },
+    {
+      name: "turc",
+      img: "images/turc.jpg"
     }
   ];
 
